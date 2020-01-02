e61f20916676079049152230656e71d345bc7271
zhaoqy123 <1564659796@qq.com>
2020-01-02 10:09:58 +0800

    通道

---
+++ Future.Projct/Future.BLL/CustomRewriteMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace Future.BLL
+{
+    public class CustomRewriteMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        //Your constructor will have the dependencies needed for database access
+        public CustomRewriteMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var path = context.Request.Path.ToUriComponent().ToLowerInvariant();
+            var thingid = context.Request.Query["thingid"].ToString();
+            var parm = path.Replace("/", "");
+            QueryString qString = new QueryString("?test=" + parm);
+            context.Request.QueryString = qString;
+           // var templateController = GetControllerByThingid(thingid);
+            var pathnew = @"/home/index";
+            context.Request.Path = pathnew;
+            await _next.Invoke(context);
+
+        }
+    }
+}
